c32553b19bd9bbdf578ccf2bfa01fdca29a09663
Ghraz-hub <tomas.dan.carlsson@hotmail.com>
2026-09-04 11:02:50 +0200

    Implement customer service interface

---
+++ CustomerManager.ConsoleApp/Customers/Services/InMemoryCustomerService.cs
@@ -0,0 +1,27 @@
+﻿using CustomerManager.ConsoleApp.Customers.Models;
+
+namespace CustomerManager.ConsoleApp.Customers.Services;
+
+public class InMemoryCustomerService : ICustomerService
+{
+    private readonly List<Customer> _customerList = [];
+
+    private static Customer CreateCustomer(string name, string email) => new(Guid.NewGuid(), name.Trim(), email.ToLower());
+    public Customer AddCustomer(string name, string email)
+    {
+        var customerId = Guid.NewGuid();
+
+        var customer = new Customer(customerId, name, email);
+
+        _customerList.Add(customer);
+
+        return customer;
+    }
+
+    public IReadOnlyList<Customer> GetAllCustomers()
+    {
+        return _customerList
+            .OrderBy(customer => customer.Name)
+            .ToList();
+    }
+}
